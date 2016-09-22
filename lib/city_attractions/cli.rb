f1bcf6b35ed54e1a8bdc95763c03933ed69f77a5
Minh Nguyen <mhnguyen289@gmail.com>
2016-09-21 17:34:05 -0700

    added choose_city method

--- lib/city_attractions/cli.rb
+++ lib/city_attractions/cli.rb
@@ -2,6 +2,8 @@ class CityAttractions::CLI
   def call
     puts 'welcome'
     list_cities
+    choose_city
+    
   end
 
   def list_cities
@@ -11,4 +13,18 @@ class CityAttractions::CLI
     end
   end
 
+  def choose_city
+      puts "Choose the city number for its top attractions"
+      city_choice = nil
+      city_choice = gets.strip.downcase
+      city = CityAttractions::City.all[city_choice.to_i - 1]
+      CityAttractions::Scraper.scrape_attractions(city)
+      puts "here are the top attractions for #{city.name}"
+
+  end
+
+
 end
+
+
+
