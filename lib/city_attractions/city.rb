a10401af8849269b2db0856efdb0ae2c0c92f452
Minh Nguyen <mhnguyen289@gmail.com>
2016-09-21 17:33:23 -0700

    added attractions array to store all city attractions

--- lib/city_attractions/city.rb
+++ lib/city_attractions/city.rb
@@ -1,9 +1,10 @@
 class CityAttractions::City
-  attr_accessor :name, :url
+  attr_accessor :name, :url, :attractions
   @@all = []
   def initialize(name, url)
     @name = name
     @url = url
+    @attractions = [] 
     @@all << self
   end
 
