1dbc34152928d86d1c54884128598b31e51fc5eb
Minh Nguyen <mhnguyen289@gmail.com>
2016-09-21 17:35:22 -0700

    added initialize method with name and url instances

--- lib/city_attractions/attraction.rb
+++ lib/city_attractions/attraction.rb
@@ -1,3 +1,17 @@
-class Attraction
-  attr_accessor :name
+class CityAttractions::Attraction
+  attr_accessor :name, :url, :city
+
+  @@attractions = []
+
+  def initialize(name, url)
+  	@name = name
+  	@url = url
+  	save
+  end
+
+  def save
+  	@@attractions << self
+  end
+
+
 end
