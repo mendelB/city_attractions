b94cbb86cf08bab4ee95134b01b2cfc9d1b6230f
Minh Nguyen <mhnguyen289@gmail.com>
2016-09-22 15:50:12 -0700

    added errors

--- lib/city_attractions/cli.rb
+++ lib/city_attractions/cli.rb
@@ -15,27 +15,44 @@ class CityAttractions::CLI
   end
 
   def choose_city
-      puts "Choose the city number for its top attractions"
       city_choice = nil
-      city_choice = gets.strip.downcase
-      city = CityAttractions::City.all[city_choice.to_i - 1]
-      @city_attractions = CityAttractions::Scraper.scrape_attractions(city)
-      puts "here are the top attractions for #{city.name}"
-      @city_attractions.each_with_index do |attraction,index|
-        puts "#{index + 1}. #{attraction.name}"
-      end
-       puts "choose an attraction for more info"
-  end
+      
+        
+        puts "Choose the city number for its top attractions"
+        city_choice = gets.strip.downcase
+        if city_choice == 'exit'
+          puts 'Now exiting program.'
+        elsif city_choice.to_i <= CityAttractions::City.all.size
+          city = CityAttractions::City.all[city_choice.to_i - 1]
+          @city_attractions = CityAttractions::Scraper.scrape_attractions(city)
+          puts "here are the top attractions for #{city.name}"
+          @city_attractions.each_with_index do |attraction,index|
+            puts "#{index + 1}. #{attraction.name}"
+          end
+        else
+          puts "sorry I didn't get that"
+        end
+      
+    end
 
   def list_attractions
     attraction_choice = nil
-    attraction_choice = gets.strip.downcase
-    attraction = CityAttractions::Attraction.all[attraction_choice.to_i - 1]
-     @attraction_info = CityAttractions::Scraper.scrape_info(attraction)
-       @attraction_info.collect do |info|
-       puts "#{info}"
-       end 
+    until attraction_choice == 'exit'
+      puts "choose an attraction for more info or choose exit to leave the program"
+      attraction_choice = gets.strip.downcase
+      if attraction_choice == 'exit'
+        puts 'Now exiting program.'
+      elsif attraction_choice.to_i <= CityAttractions::Attraction.all.size
+        attraction = CityAttractions::Attraction.all[attraction_choice.to_i - 1]
+        @attraction_info = CityAttractions::Scraper.scrape_info(attraction)
+        @attraction_info.collect do |info|
+          puts "#{info}"
+        end
+      else
+        puts "sorry I didn't get that"
+      end
     end
+  end
 end
 
 
